b62408b01d6119a350085bb64ac84ed2e0bb4740
Unknown <chamayou.guilhem13@gmail.com>
2019-02-28 11:14:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/Crypto.java
+++ src/Crypto.java
@@ -10,6 +10,8 @@ public class Crypto {
             if (args[1].equals("in")) {
                 byte[] encoded = Files.readAllBytes(Paths.get(args[2]));
                 System.out.println(Arrays.toString(encoded));
+                EnDeCryption cryption = new EnDeCryption();
+                System.out.println(Arrays.toString(cryption.encryption(encoded)));
                 //Affichage binaire
                 BigInteger bi = new BigInteger(encoded);
                 System.out.println("Data 1 : " + bi.toString(2) + " \n Nombre de bits : " + bi.toString(2).getBytes().length);
--- src/EnDeCryption.java
+++ src/EnDeCryption.java
@@ -5,18 +5,31 @@ import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
+import java.util.Arrays;
+
 import static javax.crypto.Cipher.DECRYPT_MODE;
 
 public class EnDeCryption {
 
-    String password = "";
+    String password = "key-secret";
 
     public byte[] encryption(byte[] data){
         try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, generateKey(password));
-            cipher.update(data);
-            return cipher.doFinal();
+
+            byte[] cipher_data = cipher.update(data);
+            byte[] rest = cipher.doFinal();
+            byte[] result = new byte[cipher_data.length + rest.length];
+            System.arraycopy(cipher_data, 0, result, 0, cipher_data.length);
+            System.arraycopy(rest, 0, result, cipher_data.length, rest.length);
+            System.out.println();
+            System.out.println(Arrays.toString(cipher_data));
+            System.out.println(Arrays.toString(rest));
+            System.out.println((Arrays.toString(result)));
+            System.out.println();
+
+            return result;
         } catch (Exception ex){
             ex.printStackTrace();
         }
@@ -25,7 +38,7 @@ public class EnDeCryption {
 
     public byte[] decryption(byte[] data){
         try{
-            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(DECRYPT_MODE, generateKey(password));
             cipher.update(data);
             return cipher.doFinal();
@@ -41,7 +54,7 @@ public class EnDeCryption {
             SecureRandom random = SecureRandom.getInstanceStrong();
             random.nextBytes(salt);
             SecretKeyFactory factory =
-                    SecretKeyFactory.getInstance("NoPadding");
+                    SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 10000, 128);
             SecretKey tmp = factory.generateSecret(spec);
             return new SecretKeySpec(tmp.getEncoded(), "AES");
